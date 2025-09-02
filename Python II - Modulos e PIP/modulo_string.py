5979e320e470872ceb1dd09318c5f562dc348667
Gustavo lima <129420909+gustavolima37@users.noreply.github.com>
2025-09-02 15:32:50 -0300

    Update modulo_string.py

--- Python II - Modulos e PIP/modulo_string.py
+++ Python II - Modulos e PIP/modulo_string.py
@@ -1,10 +1,10 @@
 #Utilizando ajuda da aula Slice.py de Python I
 
 def invert(string):
-    return string[::-1]
+    return string[::-1]#passo negativo, regressa: 3 2 1 0
 
 def caracter_par(string):
-    return string[::2]
+    return string[::2] #passo de 2 em 2: 0 2 4 6
 
 def caracter_impar(string):
-    return string[1::2]
+    return string[1::2]#inicia no 1 e passo 2 em 2: 1 3 5 7
