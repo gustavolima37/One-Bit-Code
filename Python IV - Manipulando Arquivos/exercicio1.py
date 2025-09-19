6ae5d381e34502e996ca0b40f4b4b516b8f7002d
Gustavo lima <129420909+gustavolima37@users.noreply.github.com>
2025-09-19 14:13:49 -0300

    Atualizando pra encerrar por hj

--- Python IV - Manipulando Arquivos/exercicio1.py
+++ Python IV - Manipulando Arquivos/exercicio1.py
@@ -5,3 +5,24 @@ Lendo n linhas de um arquivo
 ser passadas via parâmetro na função.
 '''
 
+# eu:
+
+# def ler_n_linhas(arquivo, numero_linhas):
+#     with open(arquivo, 'r', encoding='utf-8') as file:
+#         for i, linha in enumerate(file):
+#             if i >= numero_linhas:
+#                 break
+#             print(f'N.{i+1} - {linha.strip()}')
+            
+# ler_n_linhas('dados/names.txt', 4)
+#_________________________________________________________
+
+# Prof.
+
+def file_read_from_line(fname, nlines):
+    from itertools import islice
+    with open(fname, encoding='utf-8') as file:
+        for line in islice(file, nlines):
+            print(line.strip())
+            
+file_read_from_line('dados/frases.txt', 2)
