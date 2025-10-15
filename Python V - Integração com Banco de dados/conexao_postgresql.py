bb53f931c2ffa4cff16a9d45093c493422fbda53
Gustavo lima <129420909+gustavolima37@users.noreply.github.com>
2025-10-15 12:28:17 -0300

    Att

--- Python V - Integração com Banco de dados/conexao_postgresql.py
+++ Python V - Integração com Banco de dados/conexao_postgresql.py
@@ -3,13 +3,19 @@ Nessa aula vamos aprender a ler dados da tabela do PostgreSQL utilizando a lingu
 Precisamos instalar a biblioteca psycopg2 com o comando pip install psycopg2.
 '''
 import psycopg2
+import os
+from dotenv import load_dotenv
 
+# Carrega variáveis do .env
+load_dotenv()
+
+# Conecta ao banco usando variáveis de ambiente
 conn = psycopg2.connect(
-    database = 'fliperama',
-    user = 'postgres',
-    password = '1234', #use a senha que vc colocou na instalação.
-    host = 'localhost',
-    port = '5432'
+    database = os.getenv("DB_NAME"),
+    user = os.getenv("DB_USER"),
+    password = os.getenv("DB_PASSWORD"), # senha guardada
+    host = os.getenv("DB_HOST"),
+    port = os.getenv("DB_PORT")
 )
 
 '''
